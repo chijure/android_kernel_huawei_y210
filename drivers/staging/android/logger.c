7eacb1aaca4dc23dca67d3f2ca42afe379776185
Jhuan Reategui Solis <reateguisolisjm@gmail.com>
2025-12-13 18:41:13 -0500

    Disable Huawei logger check for development

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -612,7 +612,7 @@ static int __init logger_init(void)
 	int ret;
 
 	/*add user log on/off switch*/
-    #ifdef CONFIG_HUAWEI_KERNEL
+    #if 0  /* Disabled Huawei logger check for development */
 	u16 nv_item = LOG_CTL_INFO_ITEM;
 	struct log_ctl ctl_info;
 	int  rval = -1;
